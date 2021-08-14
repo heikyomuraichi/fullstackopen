025941ee339acaab7fdd750dfbf7b7c7a27e81a6
ichimura kyohei <nacsan654@gmail.com>
2021-08-14 19:02:49 +0900

    日本語の格言に変更して日本語化

--- exercises/1-4/src/App.js
+++ exercises/1-4/src/App.js
@@ -6,13 +6,23 @@ const Button = (props) => (
 )
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+    '一日は２４時間ある。今日中という意味は明日の朝までという意味である。',
+    'プログラムは思った通りに動かない。書いた通りに動く。',
+    '要求仕様はプログラム完成後に完結する。基本仕様は完成品を顧客が見てから決定される。詳細仕様は使用者がプログラムを動かしてから固まる。',
+    '私は、ソフトウェア設計には 二つの方法があるという結論に達した。一つは、欠陥がないことが明らかなほど単純にする方法である。もう一つは、明らかな欠陥がないほど複雑にする方法である。',
+    'コードは開発現場で書くんじゃない！　納品先で書くんだ！デバグは納期前にするんじゃない！　運用後にするんだ！！',
+    '画面は青かった 。',
+    '「無理です！」は言ったもん勝ち',
+    '顧客は最悪を考えず、最悪に対応しようとするのを悪質な費用のぼったくりだと思う。ＳＥは最悪に備え、最悪に対応しようとする。ＰＧは最悪を誰よりも予想し、最悪を無視する。'
+  , 'バグを起こさない唯一の方法は プログラムをくまないことだ。次善の方法は、スケジュールと人員を定めた後は仕様の変更や追加の度にプロジェクトを見直すことだ。'
+  , '設計者が、設計以上のものを 製造者に求める分野なんてあるはずがないじゃないか。'
+  , '仕様書に書かれていないことをしても、また、仕様書に書かれていることだけしてもＳＥはプログラマーに文句を言うものだ。だからプログラマーは仕様書に書かれていることだけしかしない。'
+  , 'ＳＥがプログラマーに言う「常識」は、三時間毎に変化する。'
+  , '自分の仕様書を読んでください。動かないのが仕様です。'
+  , '無理ですというのは一日を何時間で計算しているんだ。一日は ３人日、一ヶ月は４．５人月あるんだぞ。'
+  , '綺麗な仕様では、バグが出ないとわかったの。汚れているのは、 設計なんです。何故こんなことに・・・'
+  , '問題に対してすぐさま解決法を決めつけるプログラマが多すぎる。彼らは 1分考えて、1日をコーディングに費やす。1時間考えて 1時間でコーディングする代わりに。Jon Bentley'
+  
   ]
   
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
@@ -32,18 +42,18 @@ const App = () => {
   const maxidx = votes.indexOf(max);
   return (
     <div>
-      <h3>Anecdote of the day</h3>
+      <h3>今日の格言</h3>
       <div>
         {anecdotes[selected]}
       </div>
       <div>
-        has {votes[selected]} votes
+         {votes[selected]} 票
       </div>
       <div>
-        <Button  handleClick={createVotes()} text="vote"></Button>
-        <Button  handleClick={createRandom()} text="next anecdote"></Button>
+        <Button  handleClick={createVotes()} text="投票"></Button>
+        <Button  handleClick={createRandom()} text="次の格言"></Button>
       </div>
-      <h3>Anecdote with most votes</h3>
+      <h3>最も人気のある格言</h3>
       <div>
         {anecdotes[maxidx]}
       </div>
